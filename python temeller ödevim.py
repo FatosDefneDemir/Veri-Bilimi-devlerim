e3e21e265c1db90f844d5b038ea1cc182c2b8c2e
FatosDefneDemir <75983998+FatosDefneDemir@users.noreply.github.com>
2021-02-08 14:21:28 +0300

    Add files via upload

---
+++ python temeller ödevim.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+anapara=1000
+b= 12/100
+gün=7
+print(anapara*(1+b)**gün,"$")
+print("anapara ve faiz toplam tutarıdır.")
+
+
+# In[24]:
+
+
+a=1000
+b="%12"
+c=1210.68
+metin="Hafta başında {} dolarlık bitcoin aldığımızda günde ortalama {} kazançla, bir hafta sonunda {} dolar kazanırdık"
+print(metin.format(a,b,c))
+
+
+# In[ ]:
+
+
+
+
